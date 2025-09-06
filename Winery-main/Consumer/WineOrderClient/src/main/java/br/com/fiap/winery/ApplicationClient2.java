aa9eba1f03fb58d7544fed31d5c3133416bbae36
PedroHMellow <143191198+PedroHMellow@users.noreply.github.com>
2025-09-06 19:25:36 -0300

    Update ApplicationClient2.java

--- Winery-main/Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
+++ Winery-main/Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
@@ -6,27 +6,32 @@ public class ApplicationClient2 {
     public static void main(String[] args) {
 
         try {
-            WineStockServiceImplementationService service = new WineStockServiceImplementationService();
+            WineStockServiceImplementationService stockServiceFactory = new WineStockServiceImplementationService();
+            WineStockService stockPort = stockServiceFactory.getWineStockServiceImplementationPort();
 
-            WineStockService port = service.getWineStockServiceImplementationPort();
+            System.out.println("Conectando aos serviços...");
 
-            System.out.println("Conectando ao serviço de estoque de vinhos...");
-
-            String menu = port.getMenu();
+            String menu = stockPort.getMenu();
             System.out.println(menu);
 
             Scanner scanner = new Scanner(System.in);
 
             System.out.print("Digite o nome do vinho que deseja pedir: ");
             String wineName = scanner.nextLine();
-
+            
             System.out.print("Digite a quantidade desejada: ");
             int quantity = scanner.nextInt();
 
-            System.out.println("\nEnviando seu pedido...");
-            String orderStatus = port.placeOrder(wineName, quantity);
-
-            System.out.println("Resposta do servidor: " + orderStatus);
+            if (quantity > 10) {
+                WineWarningServiceImplementationService warningServiceFactory = new WineWarningServiceImplementationService();
+                WineWarningService warningPort = warningServiceFactory.getWineWarningServiceImplementationPort();
+                String warningMessage = warningPort.sendWarn();
+                System.out.println("\nResposta do servidor de avisos: " + warningMessage);
+            } else {
+                System.out.println("\nEnviando seu pedido...");
+                String orderStatus = stockPort.placeOrder(wineName, quantity);
+                System.out.println("Resposta do servidor: " + orderStatus);
+            }
 
             scanner.close();
         } catch (Exception e) {
